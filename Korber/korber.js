bc1ff67a3eb1a20abc4c86377fd568aceea2dc64
AttiqaNazir <atiqanazir15@gmail.com>
2022-02-16 11:30:00 +0500

    feat: get form values

---
+++ Korber/korber.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#btn").click(function(){
+  var firstName=$("#firstName").val();
+  var lastName = $("#lastName").val();
+  var company = $("#company").val();
+  var email = $("#email").val();
+  var country = $("#country").val();
+  var language = $("#language").val();
+  return false;
+  });
+});
