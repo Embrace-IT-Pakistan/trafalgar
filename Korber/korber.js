308f15fa214fb4cda42e43baa1c103fc77cd0e5c
AttiqaNazir <atiqanazir15@gmail.com>
2022-02-16 17:31:56 +0500

    refactor:jquery code refactor

--- Korber/korber.js
+++ Korber/korber.js
@@ -1,15 +1,29 @@
-$(document).ready(function(){
-  $("#btn").click(function(){
-  var firstName=$("#firstName").val();
-  var lastName = $("#lastName").val();
-  var company = $("#company").val();
-  var email = $("#email").val();
-  var country = $("#country").val();
-  var language = $("#language").val();
-  $('#tableBody').append(
-    "<tr><td>"+firstName+"</td>"+
-    "<td>"+lastName+"</td>"+"<td>"+company+"</td>"+"<td>"+email+"</td>"+"<td>"+country+"</td>"+"<td>"+language+"</td>"+"</tr>"
-  )
-  return false;
-  });
-});
+$(document).ready(function () {
+  $('#user-form').submit(function (e) {
+    e.preventDefault()
+    const firstName = $(this).find('input[name="firstName"]').val()
+    const lastName = $(this).find('input[name="lastName"]').val()
+    const company = $(this).find('input[name="company"]').val()
+    const email = $(this).find('input[name="email"]').val()
+    const country = $(this).find('select[name="country"]').val()
+    const language = $(this).find('select[name="language"]').val()
+    const selectedCountry = $(this)
+      .find('select[name="country"]')
+      .find(`option[value="${country}"]`)
+      .text()
+    const selectedLanguage = $(this)
+      .find('select[name="language"]')
+      .find(`option[value="${language}"]`)
+      .text()
+    $('#tableBody').append(
+      `<tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${company}</td>
+        <td>${email}</td>
+        <td>${selectedCountry}</td>
+        <td>${selectedLanguage}</td>
+      </tr>`
+    )
+  })
+})
